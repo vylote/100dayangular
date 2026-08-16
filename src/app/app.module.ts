084fe9b8410489d3b352bd21786b9d58f4a69466
vylote <antran064969@gmail.com>
2026-08-16 19:45:51 +0700

    day5: ngFor

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,11 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-import { HelloComponent } from './hello/hello.component';
-import { HiComponent } from './hi/hi.component';
 import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
-    AppComponent, HelloComponent, HiComponent
+    AppComponent
   ],
   imports: [
     BrowserModule, FormsModule
