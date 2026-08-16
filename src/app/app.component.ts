b25aae79560f3a50a7fda8f69e53e8221e8153ee
vylote <antran064969@gmail.com>
2026-08-16 14:23:22 +0700

    day3: data, event, 2waybinding

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, VERSION } from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +7,21 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'angular100day';
+  name = 'Angular' + VERSION.major
+
+  //data binding
+  inputType ='text'
+
+  //event binding
+  handler(event) {
+    console.log('click', event)
+  }
+
+  user = {
+    name: 'Vy',
+    age: 21
+  };
 }
+
+/* TS class (AppComponent) không biết th TS decorator là gì, ví dụ là nếu thêm private vào trước user thì templateUrl: k 
+thể gọi dc các att của user */
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,18 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello/hello.component';
 import { HiComponent } from './hi/hi.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
     AppComponent, HelloComponent, HiComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule, FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
+
+// FormModule: làm việc với form directive
