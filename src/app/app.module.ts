aa9b45c6a5048b16aa1bc0d2e301978b729f9edd
vylote <antran064969@gmail.com>
2026-08-16 13:25:56 +0700

    day2

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HelloComponent } from './hello/hello.component';
+import { HiComponent } from './hi/hi.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent, HelloComponent, HiComponent
   ],
   imports: [
     BrowserModule
