39fb2ad362f44f928e0e46ba8d8eb2d1ee670081
vylote <antran064969@gmail.com>
2026-08-17 15:17:10 +0700

    day10: viewchild and view children

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component, VERSION } from '@angular/core';
+import { Component, ElementRef, QueryList, VERSION, ViewChild, ViewChildren, ViewContainerRef } from '@angular/core';
+import { ToggleComponent } from './toggle/toggle.component';
 
 @Component({
   selector: 'app-root',
@@ -6,6 +7,26 @@ import { Component, VERSION } from '@angular/core';
   styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
+  // Truy vấn Component Con để lấy thực thể ToggleComponent
+  @ViewChildren(ToggleComponent) toggleComps: QueryList<ToggleComponent>;
+
+  // Truy vấn thẻ HTML thông thường để lấy đối tượng ElementRef bọc ngoài HTMLDivElement
+  @ViewChild('chartContainer') container: ElementRef<HTMLDivElement>;
+
+  @ViewChild('toggleBtn', {static: true}) toggleBtn: ElementRef<HTMLButtonElement>
+
+  @ViewChild('nameInput', {static: true}) nameInput: ElementRef<HTMLInputElement>
+
   name = 'Angular' + VERSION.major;
   checked = false
+  ShowLast = true
+
+  ngAfterViewInit() {
+    this.toggleComps.changes.subscribe(console.log)
+    console.log(this.container)
+  }
+
+  ngOnInit() {
+    console.log(this.container)
+  }
 }
